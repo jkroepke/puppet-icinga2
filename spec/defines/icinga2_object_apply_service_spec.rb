416411748cdc1dec9e05bc2ef42064790604dda5
Jan <kroepke@outlook.com>
2016-01-18 22:59:37 +0100

    Write my first puppet spec tests ...

--- spec/defines/icinga2_object_apply_service_spec.rb
+++ spec/defines/icinga2_object_apply_service_spec.rb
@@ -24,13 +24,13 @@ describe 'icinga2::object::apply_service', :type => :define do
       it { should_not contain_file(object_file).with_content(/^\s*check_command = "check_http"/) }
 
     end
-    context "on #{name} with custom_prepend parameter in string format" do
+    context "on #{name} with custom_prepend parameter" do
       let :facts do
         facts
       end
       let :params do
       {
-        :custom_prepend => 'vars += config'
+        :custom_prepend => ['vars += config']
       }
       end
       let :pre_condition do
@@ -50,7 +50,7 @@ describe 'icinga2::object::apply_service', :type => :define do
       it { should contain_file(object_file).with_content(/^\s*check_command = "check_http"/) }
       it { should contain_file(object_file).with_content(/^\s*vars += config/) }
     end
-    context "on #{name} with custom_append parameter in array format" do
+    context "on #{name} with custom_append parameter" do
       let :facts do
         facts
       end
@@ -80,6 +80,21 @@ describe 'icinga2::object::apply_service', :type => :define do
       it { should contain_file(object_file).with_content(/^\s*check_command = "check_http"/) }
       it { should contain_file(object_file).with_content(/^\s*vars += config/) }
       it { should contain_file(object_file).with_content(/^\s*if (host.vars.notification_type == "sms") \{\n\s+command = "sms-host-notification"\n\s+\} else \{\n\s+command = "mail-host-notification"\n\s+\}/) }
+    end
+    context "on #{name} with invalid custom_append parameter" do
+      let :facts do
+        facts
+      end
+      let :params do
+      {
+        :custom_prepend => 'invalid'
+      }
+      end
+      let :pre_condition do
+        "include 'icinga2'"
+      end
+
+      it { should raise_error(Puppet::Error, /is not an Array.\s+It looks to be a String/) }
     end
   end
 end
